b565b689eee5c4b1e1a76b81f8cc08c66366d610
Daniel Bradley <daniel@danielbradley.net>
2014-11-12 22:57:48 +0000

    Change existence from being paired to having a value
    - Think this is a better model as it gives nesting of the value and removes magic tuples.

--- ExistentialCollections/Existentialism.fs
+++ ExistentialCollections/Existentialism.fs
@@ -1,8 +1,11 @@
 ﻿namespace ExistentialCollections
 
-type Existance = 
-    | Exists
-    | Speculative
+type Existance<'a> = 
+    | Exists of 'a
+    | Speculative of 'a
+    member ex.Value = 
+        match ex with
+        | Exists value | Speculative value -> value
 
 type Awareness<'a> = 
     | Known of 'a
@@ -63,9 +66,9 @@ type ExNumber<'a> =
     | Exact of 'a
     | Estimation of Estimation<'a>
 
-type ExList<'a> = (Existance * 'a) list
+type ExList<'a> = (Existance<'a>) list
 
-type ExMap<'a, 'b when 'a : comparison> = Map<Existance * 'a, 'b>
+type ExMap<'a, 'b when 'a : comparison> = Map<Existance<'a>, 'b>
 
 type ExLookup<'a, 'b when 'a : comparison> = ExMap<'a, ExList<'b>>
 
@@ -74,6 +77,12 @@ module private Option =
         match opt with
         | Some v -> v
         | None -> defaultValue
+
+module Existance =
+    let map (mapping : 'a -> 'b) (source : Existance<'a>) : Existance<'b> =
+        match source with
+        | Exists x -> Exists (mapping x)
+        | Speculative x -> Speculative (mapping x)
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module Awareness =
@@ -89,57 +98,60 @@ module Awareness =
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module ExList = 
-    let ofList source : ExList<'a> = source |> List.map (fun item -> Exists, item)
+    let ofList source : ExList<'a> = source |> List.map (fun item -> Exists item)
     
     let filter (predicate : 'a -> Awareness<bool>) (source : ExList<'a>) : ExList<'a> = 
-        let folder (existance : Existance, item : 'a) (filtered : ExList<'a>) : ExList<'a> = 
-            match item |> predicate with
-            | Unknown -> (Speculative, item) :: filtered
-            | Known true -> (existance, item) :: filtered
-            | Known false -> filtered
+        let folder (item : Existance<'a>) (filtered : ExList<'a>) : ExList<'a> =
+            let value = item.Value
+            match value |> predicate with
+                | Known true -> item :: filtered
+                | Known false -> filtered
+                | Unknown -> Speculative value :: filtered
         List.foldBack folder source []
     
     let groupBy (keySelector : 'a -> Awareness<'b>) (source : ExList<'a>) : ExLookup<Awareness<'b>, 'a> = 
-        let folder (existance : Existance, item : 'a) (groups : ExLookup<Awareness<'b>, 'a>) : ExLookup<Awareness<'b>, 'a> = 
+        let folder (item : Existance<'a>) (groups : ExLookup<Awareness<'b>, 'a>) : ExLookup<Awareness<'b>, 'a> = 
+            let itemValue = item.Value
             let key = 
-                match keySelector item with
-                | Unknown -> Speculative, Unknown
-                | Known k -> Exists, Known k
+                match keySelector itemValue with
+                | Unknown -> Speculative  Unknown
+                | Known k -> Exists (Known k)
             
             let addedtoOwnGroup = 
                 match groups |> Map.tryFind key with
                 | None -> 
                     let unknownsList = 
                         groups
-                        |> Map.tryFind (Speculative, Unknown)
+                        |> Map.tryFind (Speculative Unknown)
                         |> Option.getOrDefault []
-                        |> List.map (fun (_, value) -> Speculative, value)
-                    groups |> Map.add key ((existance, item) :: unknownsList)
-                | Some list -> groups |> Map.add key ((existance, item) :: list)
+                        |> List.map (fun item -> Speculative (itemValue))
+                    groups |> Map.add key (item :: unknownsList)
+                | Some list -> groups |> Map.add key (item :: list)
             
-            match key with
-            | _, Unknown -> 
+            match key.Value with
+            | Unknown -> 
                 addedtoOwnGroup |> Map.map (fun key list -> 
-                                       if key = (Speculative, Unknown) then list
-                                       else (Speculative, item) :: list)
-            | _, Known k -> addedtoOwnGroup
+                                       if key = (Speculative Unknown) then list
+                                       else (Speculative itemValue) :: list)
+            | Known k -> addedtoOwnGroup
         List.foldBack folder source Map.empty
     
     let map (mapping : 'a -> 'b) (source : ExList<'a>) : ExList<'b> = 
-        source |> List.map (fun (ex, item) -> ex, mapping item)
+        source |> List.map (Existance.map mapping)
 
     let inline sum (source : ExList<'a>) : ExNumber<'a> when ^a : (static member ( + ) :  ^a *  ^a ->  ^a) and ^a : (static member Zero :  ^a) =
         let zero = LanguagePrimitives.GenericZero< (^a) >
-        let fold (existance:Existance, item:'a) (aggregate : ExNumber<'a>) : ExNumber<'a> =
+
+        let fold (existance : Existance<'a>) (aggregate : ExNumber<'a>) : ExNumber<'a> =
             match existance with
-            | Exists ->
+            | Exists item ->
                 match aggregate with
                 | Exact value -> Exact (value + item)
                 | Estimation (estimation) -> 
                     Estimation { ExcludingUnknowns = estimation.ExcludingUnknowns + item
                                  Minimum = estimation.Minimum + item
                                  Maximum = estimation.Maximum + item }
-            | Speculative when item <> zero ->
+            | Speculative item when item <> zero ->
                 match aggregate with
                 | Exact value -> 
                     Estimation { ExcludingUnknowns = value + item
@@ -154,7 +166,7 @@ module ExList =
 
 module ExLookup =
     let map (projection : 'k -> 'a -> 'b) (source : ExLookup<'k, 'a>) : ExLookup<'k, 'b> =
-        source |> Map.map (fun (_,key) list -> list |> ExList.map (projection key))
+        source |> Map.map (fun key list -> list |> ExList.map (projection key.Value))
 
     let inline sum (source : ExLookup<'a, 'b>) : ExMap<'a, ExNumber<'b>> when ^b : (static member ( + ) :  ^b *  ^b ->  ^b) and ^b : (static member Zero :  ^b) =
         source
