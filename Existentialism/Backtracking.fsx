b00d26eb46d94591a476276af34096ef033948e9
Daniel Bradley <daniel@danielbradley.net>
2016-05-14 02:01:20 +0100

    Hack Backtracking and domain models with lazies

---
+++ Existentialism/Backtracking.fsx
@@ -0,0 +1,111 @@
+﻿module Backtracking
+
+type FieldName = FieldName of string
+
+// A tree with at least one element in each branch
+type NonEmptyList<'T> =
+  {
+    Head : 'T
+    Tail : 'T list
+  }
+
+module NonEmptyList =
+  let singleton item =
+    {
+      Head = item
+      Tail = []
+    }
+
+  let append x y =
+    { x with Tail = List.append x.Tail (y.Head :: y.Tail) }
+
+  let collect lists =
+    match lists with 
+    | [] -> invalidArg "lists" "Cannot be an empty list"
+    | head :: tail ->
+      tail |> List.fold append head
+
+type Awareness<'a> = 
+  | Known of 'a
+  | Unknown of trace:FieldName NonEmptyList
+    static member inline get_Zero () : Awareness<'b> =
+        Known (LanguagePrimitives.GenericZero< (^b) >)
+    static member inline DivideByInt (x : Awareness<'b>) (y : int) : Awareness<'b> =
+        match x with
+        | Known value -> Known <| LanguagePrimitives.DivideByInt< (^b) > value y
+        | Unknown trace -> Unknown trace
+    static member inline (+) (x, y) = 
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue + yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (-) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue - yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (*) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue * yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (/) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue / yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (%) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue % yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (&&&) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue &&& yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (|||) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue ||| yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (^^^) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue ^^^ yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (<<<) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue <<< yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (~~~) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue ~~~ yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+    static member inline (>>>) (x, y) =
+        match x, y with
+        | Known xValue, Known yValue -> Known(xValue >>> yValue)
+        | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+        | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
+
+module Awareness =
+  let all (source : Awareness<'a> list) : Awareness<'a list> =
+    let unknownTraces =
+      source
+      |> List.choose (function
+        | Known _ -> None
+        | Unknown trace -> Some trace)
+    if unknownTraces <> [] then
+      Unknown(NonEmptyList.collect unknownTraces)
+    else
+      Known(source |> List.map (function
+        | Known x -> x
+        | _ -> invalidOp "Found unknowns after check."))
+
+  let both x y =
+      match x, y with
+      | Known xValue, Known yValue -> Known(xValue, yValue)
+      | Unknown trace, Known _ | Known _, Unknown trace -> Unknown trace
+      | Unknown xTrace, Unknown yTrace -> Unknown (NonEmptyList.append xTrace yTrace)
---
+++ Existentialism/LazyDomain.fsx
@@ -0,0 +1,90 @@
+﻿#load "Backtracking.fsx"
+open Backtracking
+
+type FieldBuilder(name : string) =
+  member this.Return(x) =
+    lazy (Known x)
+  member this.ReturnFrom(x : Lazy<Awareness<'a>>) =
+    x
+  member this.Bind(m : Lazy<Awareness<'a>>, f : 'a -> Lazy<Awareness<'b>>) =
+    lazy
+      match m.Value with
+      | Known x ->
+        match (f x).Value with
+        | Known x -> Known x
+        | Unknown trace ->
+          Unknown <| NonEmptyList.append trace (NonEmptyList.singleton (FieldName name))
+      | Unknown trace->
+        Unknown <| NonEmptyList.append trace (NonEmptyList.singleton (FieldName name))
+  member this.Bind(m : Awareness<'a>, f : 'a -> Lazy<Awareness<'b>>) =
+    lazy
+      match m with
+      | Known x ->
+        match (f x).Value with
+        | Known x -> Known x
+        | Unknown trace ->
+          Unknown <| NonEmptyList.append trace (NonEmptyList.singleton (FieldName name))
+      | Unknown trace->
+        Unknown <| NonEmptyList.append trace (NonEmptyList.singleton (FieldName name))
+
+let field name = new FieldBuilder(name)
+
+let foo =
+  field("Foo") {
+    return 1
+  }
+
+let bar =
+  field("Bar") {
+    let! foo = foo
+    let inc = foo + 1
+    return inc
+  }
+
+let bob =
+  field("Bob") {
+    return! bar
+  }
+
+type Instrument =
+  {
+    TotalSharesOutstanding : Awareness<decimal>
+  }
+
+type AssetInputFields =
+  {
+    Quantity : Awareness<decimal>
+  }
+
+type AssetCalculatedFields =
+  {
+    PercentTotalSharesOutstanding : decimal Awareness Lazy
+  }
+
+type Asset =
+  {
+    Instrument : Instrument
+    InputFields : AssetInputFields
+    CalculatedFields : AssetCalculatedFields
+  }
+
+let makeAsset instrument inputFields =
+  { Instrument = instrument
+    InputFields = inputFields
+    CalculatedFields =
+      {
+        PercentTotalSharesOutstanding =
+          field ("PercentTotalSharesOutstanding") {
+            let! quantity, totalSharesOutstanding =
+              Awareness.both (inputFields.Quantity) (instrument.TotalSharesOutstanding)
+            return quantity / totalSharesOutstanding
+          }
+      }
+  }
+
+let asset =
+  makeAsset
+    { TotalSharesOutstanding = Known 42m }
+    { Quantity = Unknown (NonEmptyList.singleton (FieldName "Quantity")) }
+
+asset.CalculatedFields.PercentTotalSharesOutstanding.Value
